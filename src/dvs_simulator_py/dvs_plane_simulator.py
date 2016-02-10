d771adefa87c18c78fa03ce9efa424dcf88c4fd1
Henri Rebecq <h.rebecq@gmail.com>
2016-02-10 10:22:26 +0100

    dvs simulator is still buggy when looking at I(t) for a specific pixel

--- src/dvs_simulator_py/dvs_plane_simulator.py
+++ src/dvs_simulator_py/dvs_plane_simulator.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python
 
 import rospy
-import rospkg
 import rosbag
-import OpenEXR
 import numpy as np
-import os.path
-import time
-import cv2
-from dvs_msgs.msg import Event, EventArray
+from dvs_msgs.msg import EventArray
 import math
 
 from dvs_simulator_py import dvs_simulator
-from dvs_simulator_py import dataset_utils
 from scipy.interpolate import RectBivariateSpline
 import scipy.ndimage
 from matplotlib import pyplot as plt
+from math import fabs
+
 
 #%% Load HD map
 
 plt.close('all')
 
-#img_path = '/home/henri/Dev/catkin_ws/src/rpg_blender/blender_synthesizer/textures/virtual_flying_room/road.jpg'
 img_path = '/home/henri/Dev/catkin_ws/src/rpg_blender/blender_synthesizer/textures/virtual_flying_room/mountain.jpg'
+#img_path = '/home/henri/Downloads/zebra_stripes_blurred.jpg'
 map_img = scipy.ndimage.imread(img_path, flatten=True) / 255.0
-
-#%%
-#plt.figure()
-#plt.imshow(map_img)
 
 #%% Prepare sliding trajectory of the DVS over the map
 
-dvs_size = (128, 128) # (height, width)
+dvs_size = (32, 32) # (height, width)
 X, Y = np.meshgrid(np.arange(0, map_img.shape[1], 1), np.arange(0, map_img.shape[0], 1)) 
 
 use_log = True
@@ -42,15 +34,11 @@ else:
     map_interpolator = RectBivariateSpline(Y[:,0], X[0,:], map_img)
 
 init_pos = (600, 650) # top-left corner of the DVS
-end_pos = (615, 615)
+end_pos = (610, 640)
+#init_pos = (70, 38) # top-left corner of the DVS
+#end_pos = (125, 68)
 
-#init_pos = (2000, 1000) # top-left corner of the DVS
-#end_pos = (2015, 1015)
-
-#init_pos = (1092, 1343) # top-left corner of the DVS
-#end_pos = (1092+100, 1343)
-
-num_steps = 150
+num_steps = 50
 
 trajectory = np.vstack([np.linspace(init_pos[0], end_pos[0], num_steps),
                         np.linspace(init_pos[1], end_pos[1], num_steps)]).T
@@ -63,21 +51,25 @@ sensor_x = np.arange(x, x + dvs_size[1])
 sensor_y = np.arange(y, y + dvs_size[0])
 init_sensor = map_interpolator(sensor_y, sensor_x)
 
-C = 0.15
+C = 0.1
 
 cur_time = 0.0
 last_pub_event_timestamp = 0.0
 
-bag = rosbag.Bag('/home/henri/Downloads/bag.bag', 'w')
+#bag = rosbag.Bag('/home/henri/Downloads/bag.bag', 'w')
 
 event_framerate = 200.0 # event packets per second
 
 reconstructed_sensor = init_sensor.copy()
-reference_values = init_sensor.copy()
-It_array = init_sensor.copy()
+
+sim = dvs_simulator.DvsSimulator(cur_time, init_sensor, C)
 
 events = []
 delta_t = 0.010
+
+u,v = 16, 16 # focus on I(t) for a specific pixel
+I_t = []
+
 for x,y in trajectory[1:]:
     
     cur_time += delta_t
@@ -85,100 +77,80 @@ for x,y in trajectory[1:]:
     print cur_time
 
     # publish events
-    if cur_time - last_pub_event_timestamp > 1.0/event_framerate:
-        event_array = EventArray()
-        event_array.header.stamp = rospy.Time(secs=cur_time)
-        event_array.width = dvs_size[1]
-        event_array.height = dvs_size[0]
-        event_array.events = events
-        
-        bag.write(topic='/dvs/events', msg=event_array, t=rospy.Time(secs=cur_time))
-        
-        events = []
-        last_pub_event_timestamp = cur_time
+#    if cur_time - last_pub_event_timestamp > 1.0/event_framerate:
+#        event_array = EventArray()
+#        event_array.header.stamp = rospy.Time(secs=cur_time)
+#        event_array.width = dvs_size[1]
+#        event_array.height = dvs_size[0]
+#        event_array.events = events
+#        
+#        bag.write(topic='/dvs/events', msg=event_array, t=rospy.Time(secs=cur_time))
+#        
+#        events = []
+#        last_pub_event_timestamp = cur_time
     
     
     sensor_x = np.linspace(x, x + dvs_size[1], endpoint=False, num=dvs_size[1])
     sensor_y = np.linspace(y, y + dvs_size[0], endpoint=False, num=dvs_size[0])
     current_sensor = map_interpolator(sensor_y, sensor_x)
     
-    # for each pixel, generate events
-    current_events = []
-    for u in range(dvs_size[1]):
-        for v in range(dvs_size[0]):
-            events_for_px = []
-            t_dt = cur_time
-            t = cur_time - delta_t
-            It = It_array[v,u]
-            It_dt = current_sensor[v,u]
-            previous_crossing = reference_values[v,u]
-            
-            
-            tol = 0.0001
-            if math.fabs(It-It_dt) > tol: 
-                pol = (It_dt >= It)
-                
-                list_crossings = []
-                all_crossings_found = False
-                cur_crossing = previous_crossing
-                while not all_crossings_found:
-                    if pol:
-                        cur_crossing = cur_crossing + C
-                        if cur_crossing > It and cur_crossing <= It_dt:
-                            list_crossings.append(cur_crossing)
-                        else:
-                            all_crossings_found = True
-                    else:
-                        cur_crossing = cur_crossing - C
-                        if cur_crossing < It and cur_crossing >= It_dt:
-                            list_crossings.append(cur_crossing)
-                        else:
-                            all_crossings_found = True
-                            
-                for crossing in list_crossings:            
-                    if(pol):
-                        assert(crossing > It and crossing <= It_dt)
-                    else:
-                        assert(crossing < It and crossing >= It_dt)
-                    
-                    te = t + (crossing-It)*delta_t/(It_dt-It)
-                    events_for_px.append(dvs_simulator.make_event(u,v,te,pol))
-                    
-                current_events += events_for_px
-                    
-                if bool(list_crossings):
-                    if pol:
-                        reference_values[v,u] = max(list_crossings)
-                    else:
-                        reference_values[v,u] = min(list_crossings)
+    I_t.append([cur_time, current_sensor[v,u]])
     
-    It_array = current_sensor.copy()
-    events = events + current_events
+    current_events = sim.update(cur_time, current_sensor)
+    events += current_events
+ 
+events = sorted(events, key=lambda e: e.ts)
+events_at_uv = []
+for e in events:
+    pol = +1 if e.polarity else -1
+
+    if fabs(e.x-u) < 0.01 and fabs(e.y-v) < 0.01:
+        events_at_uv.append([e.ts.to_sec(), e.x, e.y, pol])    
     
-    for e in current_events:
-        if e.polarity:
-            pol = +1
-        else:
-            pol = -1
-        reconstructed_sensor[e.y,e.x] += pol * C
+    reconstructed_sensor[e.y,e.x] += pol * C
     
-bag.close()
+#bag.close()
 
 
 #%%
 
 plt.figure()
 plt.subplot(141)
-plt.imshow(init_sensor, interpolation='none')
+plt.imshow(init_sensor, interpolation='none', cmap='gray')
 plt.title('Reference intensity')
 plt.subplot(142)
-plt.imshow(current_sensor, interpolation='none')
+plt.imshow(current_sensor, interpolation='none', cmap='gray')
 plt.title('Final intensity')
 plt.subplot(143)
-plt.imshow(reconstructed_sensor, interpolation='none')
+plt.imshow(reconstructed_sensor, interpolation='none', cmap='gray')
 plt.title('Reconstructed intensity')
 plt.subplot(144)
 diff = reconstructed_sensor - current_sensor
 plt.imshow(reconstructed_sensor-current_sensor, interpolation='none')
 plt.title('Difference')
 plt.colorbar()
+
+#%% 
+
+I_t = np.array(I_t)
+events_at_uv = np.array(events_at_uv)
+
+zero_contrast_level = I_t[0,1]
+reconstructed_I_t = np.cumsum(C*events_at_uv[:,3]) + zero_contrast_level
+
+plt.figure()
+plt.plot(I_t[:,0], I_t[:,1], 'r')
+plt.plot(events_at_uv[:,0], reconstructed_I_t, 'b+')
+
+npos = np.ceil((np.max(I_t[:,1])-zero_contrast_level)/C)+1
+nneg = np.ceil((zero_contrast_level-np.min(I_t[:,1]))/C)+1
+for n in range(int(npos)):
+    plt.axhline(y=zero_contrast_level+n*C, color='red', linestyle='dotted')
+for n in range(int(nneg)):
+    plt.axhline(y=zero_contrast_level-n*C, color='red', linestyle='dotted')
+for pt in I_t[:,0]:
+    plt.axvline(x=pt, color='green', linestyle='dotted')
+
+plt.xlabel('t (s)')
+plt.ylabel('log I')
+plt.title('Intensity vs time for pixel (%d,%d)' % (u,v))
